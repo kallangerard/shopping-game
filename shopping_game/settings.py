f24ad6b6978adde1f4ec1d83c9585585dd07d822
Kallan Gerard <36556982+kallangerard@users.noreply.github.com>
2026-03-29 08:33:09 +0800

    feat: add global auth annotation to Ingress and make database path configurable

--- shopping_game/settings.py
+++ shopping_game/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'shopping_game.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('DATABASE_PATH', str(BASE_DIR / 'db.sqlite3')),
     }
 }
 
